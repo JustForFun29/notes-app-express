8ca9145a454844108583a6b25a4b4464f208b60e
Dimash <DimashUmbetzhan@gmail.com>
2022-10-08 04:42:43 +0600

    first commit

---
+++ config/env.config.js
@@ -0,0 +1,14 @@
+
+
+const GITHUB_CLIENT_SECRET = "c301c36006269a5b6430199907d3e3c8aad06a6c"
+const GITHUB_CLIENT_ID = "bb9ec6bae003b51b42f3"
+const GITHUB_REDIRECT_URI = process.env.GITHUB_REDIRECT_URI
+
+export const GH_AUTH_DATA = {
+    client_secret: GITHUB_CLIENT_SECRET,
+    client_id: GITHUB_CLIENT_ID,
+    redirect_uri: GITHUB_REDIRECT_URI
+}
+
+export const MONGO_URI = "mongodb+srv://admin:123@cluster0.4vcemjq.mongodb.net/?retryWrites=true&w=majority"
+
